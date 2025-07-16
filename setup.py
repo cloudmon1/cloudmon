0480acec4bfcbab70288c916ee5409ee691e5cc8
cloudmon1 <103623406+cloudmon1@users.noreply.github.com>
2025-07-16 09:16:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,8 @@ setuptools.setup(
     description="This is a test package.",
     long_description=read_me_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cloudmon1/cloudmon",
-    packages=['cloudmon'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
