7ace30329a76289864a97b1205f20c4dd1557092
cloudmon1 <103623406+cloudmon1@users.noreply.github.com>
2025-07-17 17:19:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires='==3.9',
 )
